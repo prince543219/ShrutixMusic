ae3590024292bd5243f8fbfc333883300bc11d8e
prince543219 <surajitsingh54321@gmail.com>
2026-01-30 02:46:32 -0800

    Update settings.py

--- ShrutixMusic/plugins/bot/settings.py
+++ ShrutixMusic/plugins/bot/settings.py
@@ -182,7 +182,7 @@ from ShrutixMusic import nand
 
 REPO_VIDEO = "https://files.catbox.moe/aoafwn.mp4"
 
-@nand.on_message(filters.command(["repo", "source"]) & filters.private)
+@nand.on_message(filters.command(["repoooooooo", "77777777source"]) & filters.private)
 async def send_repo(_, message: Message):
     await message.reply_video(
         video=REPO_VIDEO,
@@ -412,3 +412,4 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
+
